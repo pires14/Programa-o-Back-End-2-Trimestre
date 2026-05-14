6d913108176780f436eb013a52c1da26c51ddb51
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-05-14 09:11:17 -0300

    Add files via upload

---
+++ atividade-herois-js/batalha.js
@@ -0,0 +1,12 @@
+/* usar arma.tolowercase(), etc*/
+let classes = document.getElementById('classe');
+let arma = document.getElementById('arma');
+let dado = Math.floor(Math.random() * 20) + 1;
+
+function lutar(){
+    if(!arma){
+        alert("ESCOLHA UMA ARMA!!");
+    }
+}
+
+
