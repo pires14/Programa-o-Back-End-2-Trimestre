9338e4cc0461b56440c37bd4080ee2d987774357
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-05-21 08:27:23 -0300

    Refactor battle logic and improve messages

--- atividade-herois-js/batalha.js
+++ atividade-herois-js/batalha.js
@@ -1,45 +1,60 @@
-/* usar arma.tolowercase(), etc*/
 function lutar(){
-let classe = document.getElementById('classe').value;
-let arma = document.getElementById('arma').value.trim();
-const log = document.getElementById('log');
+    const classe = document.getElementById('classe').value;
+    const arma = document.getElementById('arma').value.trim();
+    const log = document.getElementById('log');
 
 if(!arma){
-    log.innerHTML = "X Você esqueceu de equipar uma arma.";
-    return;}
-    
-    let dado = Math.floor(Math.random() * 20) +1;
-    let resultado = `Dado: ${dado} | <strong>${classe}<strong> | Arma: <strong> ${arma}<strong> `;
+    log.innerHTML = "⚔️Escolha uma arma⚔️";
+    return;
+}
+
+    const dado = Math.floor(Math.random() * 20) + 1;
+    let resultado = `🎲 Dado: ${dado} | <strong>${classe}</strong> com <strong>${arma}</strong>`;
 
     switch(classe){
-        case "Guerreiro":
-            if(arma.toLowerCase()=== "Espada" && dado > 5){
-            resultado += "<span class= 'sucesso'> SUCESSO: VOCÊ DECAPITOU COM UM GOLPE DE ESPADA!!!!</span>";/*Span class serve para chamar a classe do CSS */
+        case "guerreiro":
+        if(arma.toLowerCase()==="espada" && dado>5){
+            resultado += "<span class='sucesso'> VITÓRIA: Você corta o oponente ao meio com um só golpe de sua espada </span>"
+            log.innerHTML = resultado
+        
+        }else if(dado >15){
+            resultado += "<span class='sucesso'> VITÓRIA: Mesmo sem sua fiel lâmina você esmaga o oponente</span>"
+            log.innerHTML = resultado
+        
+        }else{
+            resultado += "<span class='derrota'> DERROTA: O oponente foi mais rápido, "+arma+" foi inútil</span>"
+            log.innerHTML = resultado
         }
-            else if(dado > 12){
-            resultado += "<span class= 'sucesso'>   VITÓRIA: VOCÊ ESMAGOU O INIMIGO.</span>";
+        break;
+        
+        case "mago":
+        if(arma.toLowerCase()==="cajado" && dado>8){
+            resultado += "<span class='sucesso'> VITÓRIA: Você vaporiza o oponente com um só raio de seu cajado. </span>"
+            log.innerHTML = resultado
+        
+        }else if(dado >15){
+            resultado += "<span class='sucesso'> VITÓRIA: Você esmaga o oponente mesmo sem seu fiel cajado. </span>"
+            log.innerHTML = resultado
+        
+        }else{
+            resultado += "<span class='derrota'> DERROTA: O oponente foi mais rápido, "+arma+" foi inútil</span>"
+            log.innerHTML = resultado
         }
-            else{
-            resultado += "<span class= 'derrota> DERROTA: VOCÊ MORREU!! FIM DE JOGO. Sua</span>";
-    }
-    break;
+        break;
 
-        case "Mago":
-            if(arma.toLowerCase()=== "Cajado" && dado > 8){
-                resultado += "<span class= 'sucesso'> SUCESSO: VOCÊ FARMOU AURA COM O CAJADO E VENCEU O INIMIGO!!!</span>";
-            }
-            else if(dado > 20){
-                resultado += "<span class= 'sucesso'> SUCESSO: VOCÊ DEU UM ATAQUE CRÍTICO E HUMILHOU O INIMIGO!!!!</span>";
-            }
-            else{
-                resultado += "<span class= 'derrota'> DERROTA: VOCÊ MORREU E FOI HUMILHADO POR TODOS. FIM DE JOGO.!!!!</span>"
-            }
-        case "Arqueiro":
-            if(arma.toLowerCase()=== "Arco" && dado > 10){
-                resultado += "<span class= 'sucesso'> SUCESSO: VOCÊ DERROTOU O INIMIGO E O HUMILHOU COM O ARCO!!</span>";
-            }
-            else{
-                resultado +=  "<span class= 'derrota'> DERROTA: O SEU ARCO QUEBROU E VOCÊ MORREU!!! FIM DE JOGO.</span>"
-            }
+        case "arqueiro":
+        if(arma.toLowerCase()==="arco" && dado>5){
+            resultado += "<span class='sucesso'> VITÓRIA: Você acerta em cheio a cabeça do seu oponente com uma só flecha </span>"
+            log.innerHTML = resultado
+        
+        }else if(dado >15){
+            resultado += "<span class='sucesso'> VITÓRIA: Mesmo sem seu fiel arco você esmaga o oponente</span>"
+            log.innerHTML = resultado
+        
+        }else{
+            resultado += "<span class='derrota'> DERROTA: O oponente foi mais rápido, "+arma+" foi inútil</span>"
+            log.innerHTML = resultado
+        }
+        break;
 }
 }
