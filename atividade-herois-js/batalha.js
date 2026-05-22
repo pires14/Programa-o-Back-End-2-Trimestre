f0a27f1b5c024cb2cb7b3032cd87fbdf5ec4a3b4
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-05-22 08:14:38 -0300

    Refactor battle logic in lutar function

--- atividade-herois-js/batalha.js
+++ atividade-herois-js/batalha.js
@@ -3,10 +3,10 @@ function lutar(){
     const arma = document.getElementById('arma').value.trim();
     const log = document.getElementById('log');
 
-if(!arma){
-    log.innerHTML = "⚔️Escolha uma arma⚔️";
-    return;
-}
+    if(!arma){
+        log.innerHTML = "⚔️Escolha uma arma⚔️";
+        return;
+    }
 
     const dado = Math.floor(Math.random() * 20) + 1;
     let resultado = `🎲 Dado: ${dado} | <strong>${classe}</strong> com <strong>${arma}</strong>`;
@@ -31,7 +31,6 @@ if(!arma){
         if(arma.toLowerCase()==="cajado" && dado>8){
             resultado += "<span class='sucesso'> VITÓRIA: Você vaporiza o oponente com um só raio de seu cajado. </span>";
             log.innerHTML = resultado;
-        
         }else if(dado === 15){
             resultado += "<span class='sucesso'> VITÓRIA: Você esmaga o oponente mesmo sem seu fiel cajado. </span>";
             log.innerHTML = resultado;
@@ -43,18 +42,16 @@ if(!arma){
         break;
 
         case "arqueiro":
-        if(arma.toLowerCase()==="arco" && dado>5){
+        if(arma.toLowerCase()==="arco" && dado>10){
             resultado += "<span class='sucesso'> VITÓRIA: Você acerta em cheio a cabeça do seu oponente com uma só flecha </span>";
             log.innerHTML = resultado;
-        
-        }else if(dado >15){
-            resultado += "<span class='sucesso'> VITÓRIA: Mesmo sem seu fiel arco você esmaga o oponente</span>";
-            log.innerHTML = resultado;
-        
         }else{
             resultado += "<span class='derrota'> DERROTA: O oponente foi mais rápido, "+arma+" foi inútil e você morreu!! Fim de jogo. </span>";
             log.innerHTML = resultado;
         }
         break;
-}
+        
+        default:
+            resultado = "Você ficou paralisado de medo.";
+    }
 }
