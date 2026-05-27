595866315ce07918a80413c39325815ef24a509c
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-05-27 09:50:40 -0300

    Add files via upload

--- 01/Classe_e_objeto.js
+++ 01/Classe_e_objeto.js
@@ -19,3 +19,24 @@ let pessoa = {
 };
 console.log(pessoa.nome);
 pessoa.estudar();
+
+
+/*O objeto é classificado com uma classe. */
+/*Criação da classe */
+class heroi{
+    constructor(nome,classe){
+        this.nome = nome;
+        this.classe = classe;
+    }
+}
+
+function criarHeroi(){
+    /*Capturando o que o usuário digitou/ selecionou na tela. */
+    /*O input e select tem um valor.*/
+    const nomeDigitado = document.getElementById('nomeInput').value;
+    const classeSelecionada = document.getElementById('classeSelect').value;
+
+    /*Criando o objeto dinâmicamente com os valores dos campos. */
+    const novoHeroi = new heroi(nomeDigitado, classeSelecionada);
+}
+
